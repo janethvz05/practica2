658b025c25485c7abd74a69b22f1375597b56cf6
Janeth Valadez <ajvz_janeth0505@live.com.mx>
2015-11-05 22:35:44 +0000

    Formulario de editar

--- www/js/services.js
+++ www/js/services.js
@@ -5,9 +5,9 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var chats = [];
+  var chatObj = "0";
 
   
-
   return {
     all: function() {
 
@@ -38,35 +38,38 @@ angular.module('starter.services', [])
       $cordovaSQLite.execute(db, 'DELETE FROM agenda where id = ?',[chat.id])
       .then(function(result){
           statusMessage = "Borrado";
+          chats.splice(chats.indexOf(chat), 1);
       },
       function(error){
           statusMessage = "Error: " + error.message;
       });
     },
     
-    
     get: function(chatId) {
-        chats=[];
-    $cordovaSQLite.execute(db, 'SELECT * FROM agenda where id=?',[chatId])
-         .then(function(result){
-             
-             if (result.rows.length>0){
-                 
-                 chats.push({"id":result.rows.item(0).id,
-                             "nombre":result.rows.item(0).nombre,
-                             "apellido":result.rows.item(0).apellido,
-                             "telefono":result.rows.item(0).telefono,
-                             "email":result.rows.item(0).email});
-             }
-             
-             
-    },
-    function(error){
-           statusMessage= "Error: " + error.message;
-    }
-    );
-    
-    return chats;
+
+        chats = [];
+        
+        $cordovaSQLite.execute(db, 'SELECT * FROM agenda where id = ?',[chatId])
+       .then(
+          function(result) {
+               
+             if (result.rows.length > 0) {
+                        chats.push({"id":result.rows.item(0).id,
+                                    "nombre":result.rows.item(0).nombre,
+                                    "apellido":result.rows.item(0).apellido,
+                                    "telefono":result.rows.item(0).telefono,
+                                    "email":result.rows.item(0).email});                 
+                
+                    }
+                    
+                },
+                function(error) {
+                    statusMessage = "Error on loading: " + error.message;
+                }
+        );
+
+      return chats;
+
     }
 
   };
